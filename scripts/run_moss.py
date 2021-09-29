762b56bcf1dd969bae711c722e9d0910e7b929bd
Sebastián Uriarte Güimil <sebastian.uriarteg@gmail.com>
2021-09-29 02:12:58 -0300

    Refactor previous commit's contents

--- scripts/run_moss.py
+++ scripts/run_moss.py
@@ -9,18 +9,21 @@ def get_configuration():
     return configurationParser
 
 
-userId = get_configuration()["Moss"]["userId"]
-argumentParser = ArgumentParser(
-    description="Simple script that uploads a collection of \"Aulas\"-\
-    structured student solutions to the Moss service for plagiarism \
-    checking purposes.")
-argumentParser.add_argument("--language", help="The programming \
-    language used in the assignment.", choices=Moss.languages,
-                            default="haskell")
-argumentParser.add_argument("--path", help="The path to the solutions \
-    to be processed.")
-argumentParser.add_argument("--base_path", help="Path to the base file\
-    (s) handed to students as part of the assignment.", default=None)
+def configure_arguments():
+    argumentParser = ArgumentParser(
+        description="Simple script that uploads a collection of \
+            \"Aulas\"-structured student solutions to the Moss service \
+            for plagiarism checking purposes.")
+    argumentParser.add_argument("--language", help="The programming \
+        language used in the assignment.", choices=Moss.languages,
+                                default="haskell")
+    argumentParser.add_argument("--path", help="The path to the \
+        solutions to be processed.")
+    argumentParser.add_argument("--base_path", help="Path to the base \
+        file(s) handed to students as part of the assignment.")
+    return argumentParser
 
-arguments = argumentParser.parse_args()
-moss = Moss(userId, argumentParser.language)
+
+userId = get_configuration()["Moss"]["userId"]
+arguments = configure_arguments().parse_args()
+moss = Moss(userId, arguments.language)
