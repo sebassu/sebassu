75a2a5e27ca468aaf3e7d293423e349a98e11001
Sebastián Uriarte Güimil <sebastian.uriarteg@gmail.com>
2022-03-24 16:10:33 -0300

    Implement "Downloads" path determination for Windows

--- scripts/run_moss.py
+++ scripts/run_moss.py
@@ -1,6 +1,18 @@
+import os
 from mosspy import Moss
 from argparse import ArgumentParser
+from os.path import join, expanduser
 from configparser import ConfigParser
+from winreg import OpenKey, QueryValueEx, HKEY_CURRENT_USER
+
+
+def get_downloads_path():
+    if os.name != "nt": return join(expanduser('~'), 'downloads')
+    sub_key = "SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" \
+        "\Shell Folders"
+    downloads_guid = "{374DE290-123F-4565-9164-39C4925E467B}"
+    with OpenKey(HKEY_CURRENT_USER, sub_key) as key:
+        return QueryValueEx(key, downloads_guid)[0]
 
 
 def get_configuration():
@@ -18,7 +30,7 @@ def configure_arguments():
         language used in the assignment.", choices=Moss.languages,
                                 default="haskell")
     argumentParser.add_argument("--path", help="The path to the \
-        solutions to be processed.")
+        solutions to be processed.", required=True)
     argumentParser.add_argument("--base_path", help="Path to the base \
         file(s) handed to students as part of the assignment.")
     return argumentParser
